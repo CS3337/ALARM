100083cb9e7b32e2b6d14c98809c5333579bbdd8
branlam <35716436+branlam@users.noreply.github.com>
2018-04-16 22:27:05 -0700

    Add files via upload

---
+++ models/BGBean.java
@@ -0,0 +1,36 @@
+package models;
+
+public class BGBean {
+
+	int red, green, blue;
+
+	public BGBean() {
+		this.red = 119;
+		this.green = 136;
+		this.blue = 153;
+	}
+
+	public int getR() {
+		return red;
+	}
+
+	public void setR(int r) {
+		this.red = r;
+	}
+
+	public int getG() {
+		return green;
+	}
+
+	public void setG(int g) {
+		this.green = g;
+	}
+
+	public int getB() {
+		return blue;
+	}
+
+	public void setB(int b) {
+		this.blue = b;
+	}
+}
